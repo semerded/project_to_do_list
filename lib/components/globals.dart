143095109b1ce2ae277495046439c64c068253cc
semerded <sem.van.broekhoven@gmail.com>
2023-12-19 09:45:42 +0100

    added descriptions for settings

--- lib/components/globals.dart
+++ lib/components/globals.dart
@@ -38,12 +38,13 @@ Map taskTypesActive = {
 
 Map taskPriorityColors = {Priority.none.index: Colors.grey, Priority.low.index: Colors.green, Priority.medium.index: Colors.yellow, Priority.high.index: Colors.red};
 
-Map userPresets = {"colorSchemeChoice": "Orange", "showProgrammingColor": true, "darkMode": true, "onlySearchInTitle": false, "searchInSubTasks": false};
+Map userPresets = {"colorSchemeChoice": "Orange", "showProgrammingColor": true, "darkMode": true, "onlySearchInTitle": false, "searchInSubTasks": false, "defaultTab": 1};
 
 String accentColor = userPresets["colorSchemeChoice"]; // TODO make json with app settings and add save color scheme in there
 bool showProgrammingColor = userPresets["showProgrammingColor"];
 bool darkMode = userPresets["darkMode"];
 bool onlySearchInTitle = userPresets["onlySearchInTitle"];
 bool searchInSubTasks = userPresets["searchInSubTasks"];
+int defaultTab = userPresets["defaultTab"];
 
 APPcolorScheme colorScheme = APPcolorScheme();
